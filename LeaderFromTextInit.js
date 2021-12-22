b221aeb0097cc117def0d4b7ce210884f24a1533
dmitpon <dmitpon@gmail.com>
2021-12-22 09:51:01 +0500

    Fix typo

--- LeaderFromTextInit.js
+++ LeaderFromTextInit.js
@@ -5,7 +5,7 @@ function init(basePath) {
     action.setIcon(basePath + "/LeaderFromText.svg");
     // toggle icon from gray to color when select entity(s)
     action.setRequiresSelection(true); 
-    action.setStatusTip(qsTranslate("LeaderFromText", "&LeaderFromText visible layout blocks"));
+    action.setStatusTip(qsTranslate("LeaderFromText", "&LeaderFromText from selected text"));
     action.setDefaultShortcut(new QKeySequence("T,2,L"));
     action.setDefaultCommands(["LeaderFromText", "lftxt"]);
     action.setGroupSortOrder(160000);
